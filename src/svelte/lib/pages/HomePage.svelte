4e8c1c2d37ed65dc517618e4d5e153bb5d242a83
Razvan Tanase <tangent.jotey@gmail.com>
2022-06-10 23:23:10 +0200

    Update HomePage.svelte

--- src/svelte/lib/pages/HomePage.svelte
+++ src/svelte/lib/pages/HomePage.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-import type { Writable } from "svelte/store";
+  import type { Writable } from 'svelte/store'
   export let data: Writable<{
-    clicks:number
+    clicks: number
   }>
 </script>
 
 <div class="grid justify-center text-center">
-  <div class="mt-3"></div>
-  <button class="btn" on:click={()=>{
-    $data.clicks++
-  }}>Click me</button>
+  <div class="mt-3" />
+  <button
+    class="btn"
+    on:click={() => {
+      $data.clicks++
+    }}>Click me</button
+  >
   <h1>clicks: {$data.clicks}</h1>
 </div>
