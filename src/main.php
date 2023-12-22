a5a885611bc7055377f799a22f887c11aa62140e
tncrazvan <tangent.jotey@gmail.com>
2023-12-22 20:15:02 +0100

    chore(app): updating initialization

--- src/main.php
+++ src/main.php
@@ -5,7 +5,11 @@ use CatPaw\Web\Server;
 use CatPaw\Web\Services\OpenAPIService;
 
 function main():void {
-    $server = Server::create(interfaces: "127.0.0.1:8000");
+    $server = Server::create(
+        interfaces: "127.0.0.1:8000",
+        www: "./client/dist/",
+        api: "./api/",
+    );
     $server->router->get('/openapi', #[IgnoreOpenAPI] fn (OpenAPIService $oa) => $oa->getData());
     $server->start();
 }
